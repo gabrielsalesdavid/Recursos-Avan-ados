bd3c7a17bdf1abccd342c7e2621c209ac9500212
Gabriel Sales David <51879605+gabrielsalesdavid@users.noreply.github.com>
2020-02-12 19:45:09 -0300

    Add files via upload

--- Data.java
+++ Data.java
@@ -2,6 +2,7 @@ package br.com.recursosavancadaos;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.Month;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
@@ -11,32 +12,32 @@ public class Data {
 	public static void main(String[] args) {
 		LocalDate hoje = LocalDate.now();
 		System.out.println(hoje);
-		
-		LocalDate ano_novo = LocalDate.of(2021, Month.JANUARY, 1);		
+
+		LocalDate ano_novo = LocalDate.of(2021, Month.JANUARY, 1);
 		System.out.println(ano_novo);
-		
+
 		int ano = ano_novo.getYear();
 		Month mes = ano_novo.getMonth();
-		int dia =ano_novo.getDayOfMonth();
+		int dia = ano_novo.getDayOfMonth();
 		System.out.println("O ano novo será em " + dia + " do " + mes + " no " + ano);
-		
+
 		Period periodo = Period.between(hoje, ano_novo);
 		System.out.println(periodo);
-		
-		System.out.println("Faltam " 
-		+ periodo.getYears() 
-		+ " anos " + periodo.getMonths() 
-		+ " meses e " + periodo.getDays() 
-		+ " dias para o ano novo.");
-		
+
+		System.out.println("Faltam " + periodo.getYears() + " anos " + periodo.getMonths() + " meses e "
+				+ periodo.getDays() + " dias para o ano novo.");
+
 		DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyy");
 		System.out.println(formatador);
-		
+
 		LocalDateTime agora = LocalDateTime.now();
 		System.out.println(agora);
-		
+
 		DateTimeFormatter formatador_horas = DateTimeFormatter.ofPattern("dd/MM/yyy HH:mm:ss");
 		System.out.println(agora.format(formatador_horas));
+
+		LocalTime intervalo = LocalTime.of(10, 40);
+		System.out.println(intervalo);
 	}
 
 }
